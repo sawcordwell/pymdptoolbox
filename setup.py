7f61f51601ba43c1490aa91f019c12f38d77ede6
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2012-10-29 12:40:42 +1000

    distutils data

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from distutils.core import setup
+
+setup(name="PyMDPtoolbox",
+      version="0.1",
+      description="Python Markov Decision Problem Toolbox",
+      author="Steven Cordwell",
+      author_email="steven.cordwell@uqconnect.edu.au",
+      url="http://",
+      license="Modified BSD License",
+      py_modules=["mdp"],
+      requires=["math", "numpy", "random", "time"],)
