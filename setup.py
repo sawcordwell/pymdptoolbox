dc4978c3a40812889bf97e198a08430d1edb4d5d
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-01-24 22:18:58 +1000

    change version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="PyMDPtoolbox",
-      version="0.8",
+      version="0.9",
       description="Python Markov Decision Problem Toolbox",
       author="Steven Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
