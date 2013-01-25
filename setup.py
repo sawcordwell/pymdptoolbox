6dbe527c4115cf0a479ec2d8d3b3b4366512ede9
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-01-25 15:06:03 +1000

    update to HISTORY and setup.py ready for v0.10 tag

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name="PyMDPtoolbox",
-      version="0.9",
+      version="0.10",
       description="Python Markov Decision Problem Toolbox",
       author="Steven Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
       url="http://code.google.com/p/pymdptoolbox/",
-      license="Modified BSD License",
+      license="New BSD License",
       py_modules=["mdp"],
       requires=["math", "numpy", "random", "scipy", "time"],)
