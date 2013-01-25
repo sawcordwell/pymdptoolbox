575176576236cf4dcba0b1bda8ff1aac7216371e
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-01-25 22:10:51 +1000

    HISTORY and setup.py updates for tagging v0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="PyMDPtoolbox",
-      version="0.11",
+      version="0.12",
       description="Python Markov Decision Problem Toolbox",
       author="Steven Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
