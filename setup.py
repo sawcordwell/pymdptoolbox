3eb541ec24712fc08d036ba0d3a8a2e82579170e
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-01-25 23:29:59 +1000

    history has been added ready for tag. There are some issues:
    New issue
    Summary: Class LP linear programming is not in a usable state. Needs to be fixed.
    Labels: Priority-High
    This needs to be fixed before the version number will be bumped up to 0.14. To fix this
    the cvxopt.solvers.lp functionality will have to be investigated.
    
    New issue
    Summary: Missing unittests for PolicyIterationModified
    Labels: Priority-High
    These unit tests should be implemented as soon as possible. Will not make first release without them.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="PyMDPtoolbox",
-      version="0.12",
+      version="0.13",
       description="Python Markov Decision Problem Toolbox",
       author="Steven Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
