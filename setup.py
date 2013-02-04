ef8b4164f64b825d008b8b18e9b4ee95c5332dbb
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-02-05 09:12:15 +1000

    wrote README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="PyMDPtoolbox",
-      version="0.14",
+      version="4.0alpha1",
       description="Python Markov Decision Problem Toolbox",
       author="Steven Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
