2058a71bf4bf9bf4efc62bfa8fcc5213dc0b431d
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2013-09-11 16:13:25 +1000

    use version alpha 4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name="pymdptoolbox",
-      version="4.0a3",
+      version="4.0a4",
       author="Steven A. W. Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
       url="http://code.google.com/p/pymdptoolbox/",
